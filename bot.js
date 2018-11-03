8fed0d57a70e117d059f1522ae2511e6001b5ba9
ZeroTheKing <44266197+ZeroTheKing@users.noreply.github.com>
2018-11-03 16:41:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,42 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "+";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : One`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : One ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`AFK !`,"http://twitch.tv/One")
-client.user.setStatus("dnd")
+var prefix = "d";
+var adminprefix = '*'
+const developers = ["357935587652534273"]
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(`:incoming_envelope: By 1   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`:incoming_envelope: By 1   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`:incoming_envelope: By 1   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/One");
+      message.channel.send(`:incoming_envelope: By 1`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
+
+
 
 
 
